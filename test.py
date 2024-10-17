5040e5728e717346f3f61cb0bfd3cecaad2e37c9
aaronwu2 <150697309+aaronwu2@users.noreply.github.com>
2024-10-18 00:01:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,4 @@
 print("This is our exercise3.")
-<<<<<<< HEAD
-id_input = input("Enter your id: ")
-print(f"Hello, {id_input}!")
-user_age = input("Enter your age: ")
-if user_age >= 100:
-    print("hello, elder")
-else:
-    print("You're normal")
-=======
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-print(f"The sum of {num1} and {num2} is {num1 + num2}.")
+for i in range(5):
+    print(f"Iteration {i + 1}")
 
->>>>>>> 081118bebe2ccc54a5fcfa6e390a7c7002c8ca51
