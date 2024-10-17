e37541bb2296e0db50f2cc6b727c140293f05959
benjamin474 <coding47414@gmail.com>
2024-10-17 23:47:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 print("This is our exercise3.")
 id_input = input("Enter your id: ")
 print(f"Hello, {id_input}!")
+name = input("Enter your name")
+if name >= 18:
+    print("You're an adult")
+elif name >= 0:
+    print("You're a kid")
+else:
+    print("You're not a human, who are you?")
