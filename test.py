e055da5178ccc1cc16ae3da9dc4fe5ba38da010f
benjamin474 <coding47414@gmail.com>
2024-10-18 00:01:19 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-print("This is our exercise3.")
-<<<<<<< HEAD
-id_input = input("Enter your id: ")
-print(f"Hello, {id_input}!")
-user_age = input("Enter your age: ")
-if user_age >= 100:
-    print("hello, elder")
-else:
-    print("You're normal")
-=======
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-print(f"The sum of {num1} and {num2} is {num1 + num2}.")
-
->>>>>>> 081118bebe2ccc54a5fcfa6e390a7c7002c8ca51
+name1 = input('input the first name:')
+name2 = input('input the second name:')
+print(f"Hello, {name1} and {name2}")
