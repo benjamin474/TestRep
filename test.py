5706fa81cba012186d000320ded30fbd876f471d
aaronwu2 <150697309+aaronwu2@users.noreply.github.com>
2024-10-17 23:35:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 print("This is our exercise3.")
 user_input = input("Enter your name: ")
 print(f"Hello, {user_input}!")
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
